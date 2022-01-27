367b33438fc89282729d737c79169872028568d5
oscarfiscal <oscarfiscalcp@gmail.com>
2022-01-27 17:25:11 -0500

    Inicio prueba de frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Contact',
+    component: Contact
   },
   {
     path: '/about',
